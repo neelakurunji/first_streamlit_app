7a147b9b28363ae311958644b24f8fe87bd97947
Venkatesh Dharmapuri <89900575+neelakurunji@users.noreply.github.com>
2023-10-18 12:25:29 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,5 @@
 import streamlit
 streamlit.title("Jai Shri Ram!")
+streamlit.header("Aditya Hridayam Strotram")
+streamlit.text("Tatho Yuddha Parisrantam |")
+streamlit.text("Samare Chintayasmitam ||")
