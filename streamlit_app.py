225542145dc0d6131196bdedc045f72237f1a2a8
Venkatesh Dharmapuri <89900575+neelakurunji@users.noreply.github.com>
2023-10-18 12:30:09 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,3 +3,11 @@ streamlit.title("Jai Shri Ram!")
 streamlit.header("Aditya Hridayam Strotram")
 streamlit.text("Tatho Yuddha Parisrantam |")
 streamlit.text("Samare Chintayasmitam ||")
+
+streamlit.header('Breakfast Menu')
+streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔 Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞 Avocado Toast')
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
